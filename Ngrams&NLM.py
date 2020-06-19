a234573d17700b1b4ac654120d6b1638ad690795
Velda Kiara <veldakarimi@gmail.com>
2020-06-19 16:06:43 +0300

    changes

---
+++ Ngrams&NLM.py
@@ -0,0 +1,7 @@
+""" n-gram model considers a sequence of some number (n) 
+units and calculates the probability of each unit in a 
+body of language given the preceding sequence of length n.
+n-gram probabilities with larger n values can be impressive at 
+language prediction.
+A tactic known as language smoothing can help adjust 
+probabilities for unknown words, but it isn’t always ideal."""
